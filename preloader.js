52a70108abb031b59cba944c731d65807440689b
Bojan Tubic <bojantubic2411@gmail.com>
2020-03-17 08:29:26 +0100

    Changed image format

--- preloader.js
+++ preloader.js
@@ -4,13 +4,13 @@ function preload() {
     (images[i] = new Image()), (images[i].src = preload.arguments[i]);
 }
 preload(
-  "img/img-1.webp",
-  "img/img-2.webp",
-  "img/img-3.webp",
-  "img/img-4.webp",
-  "img/img-5.webp",
-  "img/img-6.webp",
-  "img/img-7.webp",
-  "img/img-8.webp",
-  "img/img-9.webp"
+  "img/img-1.png",
+  "img/img-2.png",
+  "img/img-3.png",
+  "img/img-4.png",
+  "img/img-5.png",
+  "img/img-6.png",
+  "img/img-7.png",
+  "img/img-8.png",
+  "img/img-9.png"
 );
